d56b51ee2c4f498c723b3cb35e328f148f59406c
Jannes123 <jannes.engelbrecht2@gmail.com>
2023-03-30 11:45:00 +0200

    update

--- dust/settings.py
+++ dust/settings.py
@@ -116,11 +116,11 @@ LOGGING = {
 REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework_xml.parsers.XMLParser',
-        'rest_framework_xml.parsers.JSONParser',
+        'rest_framework.parsers.JSONParser',
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework_xml.renderers.XMLRenderer',
-        'rest_framework_xml.renderers.JSONRenderer',
+        'rest_framework.renderers.JSONRenderer',
     ],
 }
 
