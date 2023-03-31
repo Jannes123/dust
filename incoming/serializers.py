3bc30a33765197ff32733d8a34a6f39d7b222232
Jannes123 <jannes.engelbrecht2@gmail.com>
2023-03-31 18:12:35 +0200

    update

--- incoming/serializers.py
+++ incoming/serializers.py
@@ -31,11 +31,6 @@ class PayInitSerializer(serializers.ModelSerializer):
                 )
 
 
-class empty:
-    """None replacement
-    """
-    pass
-
 class ExplicitPayInitSerializer(serializers.Serializer):
     payeeUuid = serializers.CharField(max_length=36)
     payeeAccountUuid = serializers.CharField(max_length=36)
@@ -50,7 +45,33 @@ class ExplicitPayInitSerializer(serializers.Serializer):
     payeeOrderItemName = serializers.CharField(max_length=50)
     payeeOrderItemDescription = serializers.CharField(max_length=60)
 
-    def __init__(self, instance=None, data=empty, **kwargs):
+    def __init__(self, instance=None, data=None, **kwargs):
         LOGGER.debug("init explicit serializer")
         super(ExplicitPayInitSerializer, self).__init__(instance, data, **kwargs)
 
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        LOGGER.debug('create')
+        LOGGER.debug(validated_data)
+        return PayInit.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.payeeUuid = validated_data.get('payeeUuid', instance.payeeUuid)
+        instance.payeeAccountUuid = validated_data.get('payeeAccountUuid', instance.payeeAccountUuid)
+        instance.payeeRefInfo = validated_data.get('payeeRefInfo', instance.payeeRefInfo)
+        instance.payeeCategory1 = validated_data.get('payeeCategory1', instance.payeeCategory1)
+        instance.payeeCategory2 = validated_data.get('payeeCategory2', instance.payeeCategory2)
+        instance.payeeCategory3 = validated_data.get('payeeCategory3', instance.payeeCategory3)
+        instance.payeeSiteName = validated_data.get('payeeSiteName', instance.payeeSiteName)
+        instance.payeeSiteReference = validated_data.get('payeeSiteReference', instance.payeeSiteReference)
+        instance.payeeInvoiceNr = validated_data.get('payeeInvoiceNr', instance.payeeInvoiceNr)
+        instance.payeeOrderNr = validated_data.get('payeeOrderNr', instance.payeeOrderNr)
+        instance.payeeOrderItemName = validated_data.get('payeeOrderItemName', instance.payeeOrderItemName)
+        instance.payeeOrderItemDescription = validated_data.get('payeeOrderItemDescription', instance.payeeOrderItemDescription)
+        instance.save()
+        return instance
