7706114d225ae9843d7ef745db0afd93308a7010
Jannes123 <jannes.engelbrecht2@gmail.com>
2023-04-04 22:55:05 +0200

    debug choices on reques model

--- incoming/admin.py
+++ incoming/admin.py
@@ -7,23 +7,30 @@ from incoming.models import CodeFunction, PayInit, PayBuyer, PayRequest, PayDeta
 class CodeFunctionAdmin(admin.ModelAdmin):
     list_display = ('user_number', 'sponsor_number', 'timestamp')
 
-class  PayInitAdmin(admin.ModelAdmin):
-    list_display = ('payeeUuid', 'payeeAccountUuid','payeeInvoiceNr','payeeOrderNr')
+
+class PayInitAdmin(admin.ModelAdmin):
+    list_display = ('payeeUuid', 'payeeAccountUuid', 'payeeInvoiceNr', 'payeeOrderNr')
+
 
 class PayBuyerAdmin(admin.ModelAdmin):
-    list_display = ('payerName', 'payerSurname', 'payerEmail','payerMobile')
+    list_display = ('payerName', 'payerSurname', 'payerEmail', 'payerMobile')
+
 
 class PayRequestAdmin(admin.ModelAdmin):
     list_display = ('requestStatus', 'requestAmount', 'requestTokenId')
 
+
 class PayDetailsAdmin(admin.ModelAdmin):
     list_display = ('paymentAmount', 'paymentDateTime', 'paymentSystemReference', 'paymentMethod')
 
+
 class MerchantDataAdmin(admin.ModelAdmin):
-    list_display = ('merchant_uuid','merchant_account_uuid','security_key')
+    list_display = ('merchant_uuid', 'merchant_account_uuid', 'security_key')
+
 
 class ProductionPurchaseAdmin(admin.ModelAdmin):
     list_display = ('name', 'surname', 'email', 'mobile')
+
 
 admin.site.register(CodeFunction, CodeFunctionAdmin)
 admin.site.register(PayInit, PayInitAdmin)
