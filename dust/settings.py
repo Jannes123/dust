1f8afc8df56a412c568cf1b5f7eae48f9adeafc7
Jannes123 <jannes.engelbrecht2@gmail.com>
2023-05-05 09:51:04 +0000

    server side upgrade

--- dust/settings.py
+++ dust/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-fj2js-xx^qt@1qf9uz(!x(xu#s*h%2pyqs7rbj(*f7a(md#w4q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', '34.29.229.62', 'pleasetopmeup.com', 'www.pleasetopmeup.com', '*.pleasetopmeup.com']
+ALLOWED_HOSTS = ['0.0.0.0', '196.38.95.55', '196.38.95.56', '34.29.229.62', 'pleasetopmeup.com', 'www.pleasetopmeup.com', '*.pleasetopmeup.com']
 
 
 if DEBUG == True:
